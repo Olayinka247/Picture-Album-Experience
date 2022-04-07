1ec552d7e5131ea37227f73ee31780da3535acf0
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-04-07 12:08:45 +0100

    oninput function

--- index.js
+++ index.js
@@ -52,12 +52,10 @@ function displayCard(data) {
     .map(
       (card) => `<div class="col-md-4">
         <div class="card mb-4 shadow-sm">
-        <img src="${card.src.medium}" alt="" style=" width:auto; height:180px; object-fit:cover;">
+        <img src="${card.src.medium}" alt=${card.alt} style=" width:auto; height:180px; object-fit:cover;">
           <div class="card-body">
             <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              ${card.alt}
             </p>
             <div
               class="d-flex justify-content-between align-items-center"
